ab6eb57a16394f98c8175fb6ed6e8aba0253baa4
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-03-07 20:50:12 +0530

    Date Left

--- SaharaBets.py
+++ SaharaBets.py
@@ -21,6 +21,8 @@ from dateutil import parser
 
 
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.opera import OperaDriverManager
 
 IsoDate = (input("Enter Date(ISO Format): "))
 
@@ -40,7 +42,13 @@ if(yourdate.date()==currDate.date()):
 else:
     DateToSearch = "0"+(str)(yourdate.day)+"/"+"0"+(str)(yourdate.month)
 
-print(DateToSearch)
+Choice = int(input("Enter 1 for NBA , 2 for NCAAB: "))
+
+if(Choice==1):
+    val = ("https://az.saharabets.com/sports/navigation/10900.1/10901.1")
+else:
+    val = ("https://az.saharabets.com/sports/navigation/12248.1/12249.1")
+
 
 
 
@@ -48,10 +56,9 @@ print(DateToSearch)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
-val = ("https://az.saharabets.com/sports/navigation/10900.1/10901.1")
+
 
 wait = WebDriverWait(driver, 10)
-
 driver.get(val)
 
 
@@ -99,6 +106,8 @@ def FetchTeamNamesFromEvent(_event):
 for event in events:
     index += 1
     print(FetchMatchFromEvent(event, index))
+   
+
 
 
 
@@ -109,20 +118,81 @@ TeamNames = []
 TeamNames = FetchTeamNamesFromEvent(events[chosen_event-1])
 
 
+SiteData = []
+SiteData = events[chosen_event-1].findAll('div',class_="flex")
+SiteData.pop(0)
 
-dict = {'Team Name': TeamNames}
+
+
+Spread = []
+Total = []
+Money = []
+
+
+
+tempdata = SiteData[0].findNext('div',class_="currenthandicap")
+Spread.append(tempdata.get_text())
+
+tempdata = SiteData[0].find('div',class_='selectionprice')
+Spread[0] = "("+Spread[0] +")"+ tempdata.get_text()
+
+
+
+
+tempdata = SiteData[1].findNext('div',class_="currenthandicap")
+Spread.append(tempdata.get_text())
+
+tempdata = SiteData[1].find('div',class_='selectionprice')
+Spread[1] = "("+Spread[1] +")"+ tempdata.get_text()
+
+
+
+tempdata = SiteData[2].find('div',class_="selectionprice")
+Money.append(tempdata.get_text())
+
+
+tempdata = SiteData[3].find('div',class_="selectionprice")
+Money.append(tempdata.get_text())
+
+
+tempdata = SiteData[4].find('div',class_="uo-currenthandicap")
+Total.append(tempdata.get_text())
+
+tempdata = SiteData[4].find('div',class_="selectionprice")
+Total[0] = Total[0] + tempdata.get_text()
+
+tempdata = SiteData[5].find('div',class_="uo-currenthandicap")
+Total.append(tempdata.get_text())
+
+tempdata = SiteData[5].find('div',class_="selectionprice")
+Total[1] = Total[1] + tempdata.get_text()
+
+
+
+
+
+
+
+with open("output.html", "a") as f:
+     print(Spread,file=f)
+
+
+dict = {'Team Name': TeamNames,'Spread':Spread,'Total':Total,'Money':Money}
 
 df = pd.DataFrame(dict)
 df.to_csv('Bets.csv')
 
 
-#print(TeamNames)
-# print(MatchName)
-# with open("output.html", "a") as f:
-#   print(versus_name,file=f)
 # date = []
 
 # Website_date = soup.find_all("div",{"class":"date"})
 
 
 driver.quit()
+
+ # -6 -110
+  #+6 -110
+  #-235
+  #+195
+  #O 232 -110
+  #U 232 -110
