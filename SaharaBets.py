2688a1e24154965ba3a2a80ce2902418ebc3cc42
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-03-07 16:18:08 +0530

    Sahara Half Done

--- SaharaBets.py
+++ SaharaBets.py
@@ -11,10 +11,33 @@ from bs4 import BeautifulSoup
 import codecs
 
 import re
+import os
+
+import csv
+import pandas as pd
+
+import datetime
+from dateutil import parser
+
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+IsoDate = (input("Enter Date(ISO Format): "))
+
+
+yourdate = parser.parse(IsoDate)
+print(yourdate.date())
+
+
+currDate = datetime.datetime.now()
+print(currDate.date())
+
+
+
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
 
 val = ("https://az.saharabets.com/sports/navigation/10900.1/10901.1")
 
@@ -31,26 +54,66 @@ if get_url == val:
     page_source = driver.page_source
 
 
+soup = BeautifulSoup(page_source, features="html.parser")
 
-soup = BeautifulSoup(page_source,features="html.parser")
-
-div_text=[]
-div_text = soup.findAll("div",{"class":"event"})
-
-
-names=[]
-names = soup.find_all("span",{"class":"name"})
-
-date = [] 
-
-date = soup.find_all("div",{"class":"date"})
+events = []
+events = soup.findAll("div", {"class": "event"})
 
 
+index = 0
+
+
+if os.path.exists("Bets.csv"):
+  os.remove("Bets.csv")
+
+
+print("Teams Playing are:")
+
+
+def FetchMatchFromEvent(_event, _index):
+
+    team_names = []
+    team_names = _event.findAll('span', class_="name")
+    versus_name = str(_index) + ".)  " + \
+        team_names[0].get_text() + " vs " + team_names[1].get_text()
+    return versus_name
+
+
+def FetchTeamNamesFromEvent(_event):
+    team_names = []
+    team_names = _event.findAll('span', class_="name")
+
+    team_names_text = [team_names[0].get_text(), team_names[1].get_text()]
+    return team_names_text
+
+
+for event in events:
+    index += 1
+    print(FetchMatchFromEvent(event, index))
 
 
 
-with open("output.html", "a") as f:
-        print(date,file=f)
+
+chosen_event = int(input("Enter the match no."))
+
+TeamNames = []
+TeamNames = FetchTeamNamesFromEvent(events[chosen_event-1])
+
+
+
+dict = {'Team Name': TeamNames}
+
+df = pd.DataFrame(dict)
+df.to_csv('Bets.csv')
+
+
+#print(TeamNames)
+# print(MatchName)
+# with open("output.html", "a") as f:
+#   print(versus_name,file=f)
+# date = []
+
+# Website_date = soup.find_all("div",{"class":"date"})
 
 
 driver.quit()
